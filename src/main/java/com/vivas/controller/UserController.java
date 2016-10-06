636551e255349782c5cd24dd2e7283963e0cb0f4
DESKTOP-6M3LFQM\duyot <ongtheduy@gmail.com>
2016-10-06 17:03:31 +0700

    no message

--- src/main/java/com/vivas/controller/UserController.java
+++ src/main/java/com/vivas/controller/UserController.java
@@ -2,10 +2,21 @@ package com.vivas.controller;
 
 import com.vivas.dto.ResponseObject;
 import com.vivas.dto.User;
+import com.vivas.enums.Responses;
 import com.vivas.utils.Constants;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by duyot on 9/30/2016.
@@ -15,17 +26,37 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @RequestMapping(value = "/getFile/{fileName:.+}")
+    public void find(@PathVariable("fileName") String fileName,HttpServletResponse response){
+        log.info("fileName: "+ fileName);
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
+            IOUtils.copy(is,response.getOutputStream());
+            response.flushBuffer();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @RequestMapping(value = "/findUser/{userId}")
-    public User find(@PathVariable("userId") Long userId ){
+    public User find(@PathVariable("userId") Long userId,HttpServletRequest request){
         log.info("UserId: "+ userId);
+        log.info("Host: "+ request.getHeader("Host"));
         return new User("duyot","123456a@");
     }
 
-    @RequestMapping(value = "/saveUser")
-    public @ResponseBody ResponseObject  saveUser(@RequestBody User user){
+    @RequestMapping(value = "/deleteUser/{userId}")
+    public ResponseObject delete(@PathVariable("userId") Long userId ){
+        log.info("Deleting : "+ userId);
+        return new ResponseObject(Responses.ERROR.getCode(),Responses.ERROR.getName(),userId+"");
+    }
+
+    @RequestMapping(value = "/saveUser",produces = "application/json")
+    public ResponseObject  saveUser(@RequestBody User user){
         log.info("getting User:"+ user.getUsername());
-        return new ResponseObject(Constants.STATUS_OK,"SUCCESS");
+        return new ResponseObject(Responses.SUCCESS.getCode(),Responses.SUCCESS.getName(),user.getUsername());
 
     }
 }
--- src/main/java/com/vivas/enums/Responses.java
+++ src/main/java/com/vivas/enums/Responses.java
@@ -3,5 +3,19 @@ package com.vivas.enums;
 /**
  * Created by duyot on 10/4/2016.
  */
-public class Responses {
+public enum Responses {
+    SUCCESS("SUCCESS","200"),ERROR("FAIL","-1");
+    public String statusName;
+    public String statusCode;
+    Responses(String statusCode,String statusName){
+        this.statusCode = statusCode;
+        this.statusName = statusName;
+    }
+    public String getName(){
+        return this.statusName;
+    }
+    public String getCode(){
+        return this.statusCode;
+    }
+
 }
